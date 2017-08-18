0e9f7c48923b9c0d39c667db09c5cfbe8062d286
bitforce <brandon.johnson.official@gmail.com>
2017-08-18 03:59:24 -0400

    ~

--- clean.sh
+++ clean.sh
@@ -1,5 +1,10 @@
+swap='*.swp'
 class='*.class' 
 if test "find . -name $class"; then
     find . -name $class -type f -exec echo "removing '{}'" \;
     rm -rf `find . -type f -name $class`
+fi
+if test "find . -name $swap"; then
+    find . -name $swap -type f -exec echo "removing '{}'" \;
+    rm -rf `find . -type f -name $swap`
 fi
