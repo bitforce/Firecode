4d4a2c94423444198adcf9e7364cea97d4da344c
Brandon Johnson <brandon.johnson.official@gmail.com>
2017-10-19 15:35:12 -0400

    ~

--- source/lvl3/p34/Source.java
+++ source/lvl3/p34/Source.java
@@ -2,6 +2,7 @@ package source.lvl3.p34;
 import java.util.*;
 class Source {
     /* ********************************************************************* */
+    private static void rotateSquareImageCCW(int[][] matrix) {}
     /* ********************************************************************* */
     public static void main(String[] args) {}
 }
