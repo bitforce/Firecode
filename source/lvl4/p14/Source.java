c5ff93af72f46311e8ddd4908d02abef66a2333c
Brandon Johnson <brandon.johnson.official@gmail.com>
2018-03-06 21:13:24 -0500

    ~

--- source/lvl4/p14/Source.java
+++ source/lvl4/p14/Source.java
@@ -32,11 +32,44 @@ class Source {
         return max == 0 ? -1 : lvl;
     }
     /* ********************************************************************* */
+    private int findMaxSumLevel2(TreeNode root) {                    
+        if(root == null) return -1;
+        int currSum = 0, maxSum = 0;
+        int currLvl = 0, maxLvl = 0;
+        TreeNode curr = null;
+        Queue<TreeNode> q = new LinkedList<TreeNode>();
+        q.add(root);
+        q.add(null); //end of first level indicator
+        while(!q.isEmpty()) {
+            curr = q.remove();
+            // If at the end of current level, compare sum and process result
+            if(curr == null) {
+                if(currSum > maxSum) {
+                    maxSum = currSum;
+                    maxLvl = currLvl;
+                }
+                currSum = 0; //reset
+                if(!q.isEmpty())
+                    q.add(null); //end of level indicator
+                currLvl++; //begin next level
+            } else {
+                currSum += curr.data;
+                if(curr.left != null)
+                    q.add(curr.left);
+                if(curr.right != null)
+                    q.add(curr.right);
+                    
+            }
+        }
+        return maxLvl;
+    }
+    /* ********************************************************************* */
     public static void main(String[] args) {
         BinaryTree tree = new BinaryTree();
         for(int i = 0; i < args.length; i++)
             tree.add(Integer.parseInt(args[i]));
         tree.print();
         System.out.println(new Source().findMaxSumLevel(tree.root));
+        System.out.println(new Source().findMaxSumLevel2(tree.root));
     }
 }
