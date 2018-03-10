8298fd7ff8a7010bc3e6d17e6bcdb3d538875202
Brandon Johnson <brandon.johnson.official@gmail.com>
2018-03-09 23:41:55 -0500

    ~

--- source/lvl4/p16/Source.java
+++ source/lvl4/p16/Source.java
@@ -3,43 +3,29 @@ import java.util.*;
 class Source {
     /* ********************************************************************* */
     private static int minTriangleDepth(ArrayList<ArrayList<Integer>> input) {
-        /*
-         * Assume index i, all we have to do is look at i+=1 && i+=2 which 
-         * will give us the adjacent pairs in the "tree". I believe we can 
-         * do this in log time and even use a dp approach , but my concern 
-         * is not know the values ahead.
-         * 
-         * Do note that you'll have to add +1 and +2 to the difference in 
-         * position of the inner array. Example, 2->3||2->5, but we need 
-         * to take the length of the row (=2), therefore 2-1 +1 or +2.
-         *
-         * Thinking it out:
-         * 1->2, 1->3
-         * two is less
-         * 2->4, 2->5
-         */
-        for(ArrayList<Integer> a : input) { // you may wanna use simple for
-            int k = a.size();
-            for(int j = 0; j < a.size(); j++) {
-                int l = j+k+1;
-                int r = j+k+2;
-                System.out.println(l + " " + r);
-                k--;
-            }
-            System.out.println("----");
+        int[][] matrix = new int[input.size()][input.size()];
+        for(int i = 0; i < input.size(); i++) Arrays.fill(matrix[i], 0);
+        assist(input, 0, 0, matrix);
+        return matrix[0][0];
+    }
+    private static int assist(ArrayList<ArrayList<Integer>> input, int level,
+                              int index, int[][] matrix) {
+        if(level + 1 == input.size()) {
+            matrix[level][index] = input.get(level).get(index);
+            return matrix[level][index];
         }
-        return 0;
+        int left = matrix[level+1][index] == 0
+            ? assist(input, level + 1, index, matrix) 
+            : matrix[level+1][index];
+        int right = matrix[level+1][index+1] == 0
+            ? assist(input, level+1, index+1, matrix)
+            : matrix[level+1][index+1]; 
+        matrix[level][index] = input.get(level).get(index) 
+            + Math.min(left, right);
+        return matrix[level][index];
     }
     /* ********************************************************************* */
     public static void main(String[] args) {
-        /*
-         *                1
-         *               2 3
-         *              4 5 6
-         *             7 8 9 10
-         *
-         * [[1], [2,3], [4,5,6], [7,8,9,10]]
-         */
         ArrayList<ArrayList<Integer>> alist = new ArrayList<>();
         ArrayList<Integer> blist = new ArrayList<>();
         int k = 0;
